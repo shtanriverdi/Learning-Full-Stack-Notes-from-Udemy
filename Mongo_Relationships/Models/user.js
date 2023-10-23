3ab324c291a99d1a3727402d5cd52b6246017827
shtanriverdi <shtanriverdi@yahoo.com>
2023-10-23 15:35:08 +0300

    one to bagilions

--- Mongo_Relationships/Models/user.js
+++ Mongo_Relationships/Models/user.js
@@ -1,4 +1,6 @@
-const { Mongoose, default: mongoose } = require("mongoose");
+/* One to Few Example */
+
+const mongoose = require('mongoose');
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/relationshipDemo');
